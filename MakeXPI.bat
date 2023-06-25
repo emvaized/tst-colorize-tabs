ae3e61057351ceee84137a1fb6d81f8e6242913e
irvinm <irvinm@yahoo.com>
2023-06-25 12:20:45 -0400

    Fixed XPI filename

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,4 +1,4 @@
 del TST-Colorize-Tabs.xpi
 "C:\Program Files\WinRAR\winrar.exe" a TST-Colorize-Tabs.zip background.js manifest.json
 "C:\Program Files\WinRAR\winrar.exe" a -r TST-Colorize-Tabs.zip images
-ren TST-Colorize-Tabs.zip TST-Colorize.xpi
+ren TST-Colorize-Tabs.zip TST-Colorize-Tabs.xpi
