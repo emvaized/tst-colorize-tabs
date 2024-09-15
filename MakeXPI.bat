e1bd8df351da02d0a1ae7acfebe564f50debf5cc
emvaized <maximtsyba@gmail.com>
2024-09-15 07:18:59 +0200

    Update project structure

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,4 +1,4 @@
+set src_dir=src
+set xpi_file=TST-Colorize-Tabs.xpi
 del TST-Colorize-Tabs.xpi
-"C:\Program Files\WinRAR\winrar.exe" a TST-Colorize-Tabs.zip background.js manifest.json options.html options.css options.js
-"C:\Program Files\WinRAR\winrar.exe" a -r TST-Colorize-Tabs.zip _locales icons images
-ren TST-Colorize-Tabs.zip TST-Colorize-Tabs.xpi
+"C:\Program Files\WinRAR\winrar.exe" a -r -afzip %xpi_file% %src_dir%\*
